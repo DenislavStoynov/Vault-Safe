6776e4be5b1bbf0e4f1983487b40d37dc7c1fba4
Denislav <dstoynov2000@gmail.com>
2024-06-13 16:13:15 +0300

    last-updates-js

--- js/script.js
+++ js/script.js
@@ -1,4 +1,7 @@
-let app, background, door, handle, handleShadow, blink, doorOpen, doorOpenShadow;
+let app, background, door, handle, handleShadow, doorOpen, doorOpenShadow, blink1, blink2, blink3;
+let blink_texture;
+let timeCounter = 0;
+let timeText, timerInterval;
 const pathPrefix = '../assets/';
 const fileExtension = '.png';
 const assets = {
@@ -10,7 +13,7 @@ const assets = {
     handleShadowAsset: 'handleShadow',
     blinkAsset: 'blink'
 };
-
+const directions = ["clockwise", "counterclockwise"];
 window.onload = async function () {
     // Start application
     try {
@@ -21,50 +24,105 @@ window.onload = async function () {
     }
 };
 
+const getTimerFontSize = () => Math.min(background.width, background.height) * 0.016;
+
+const createCounterText = () => {
+    timeText = new PIXI.Text(formatTime(timeCounter), { fontFamily: 'Arial', fontSize: getTimerFontSize(), fill: '#ffffff' });
+    timeText.position.set(background.x - background.width * 0.203, background.y - background.height * 0.059);
+    app.stage.addChild(timeText);
+};
+
+const updateCounterText = () => {
+    timeText.text = formatTime(timeCounter);
+};
+
+const formatTime = (timeCounter) => {
+    const minutes = Math.floor(timeCounter / 60);
+    const seconds = timeCounter % 60;
+    return `${minutes}:${seconds.toString().padStart(2, '0')}`;
+};
+
+const startTimer = () => {
+    if (timerInterval) {
+        clearInterval(timerInterval);
+    }
+    timerInterval = setInterval(() => {
+        timeCounter++;
+        updateCounterText();
+    }, 1000);
+};
+
+const updateTimerStylesAndPosition = () => {
+    timeText.style.fontSize = `${getTimerFontSize()}px`;
+    timeText.position.set(background.x - background.width * 0.203, background.y - background.height * 0.059);
+};
+
 const loadAsset = async (pathPrefix, asset, fileExtension) => {
     const assetPath = `${pathPrefix}${asset}${fileExtension}`;
     const texture = await PIXI.Assets.load(assetPath);
     return texture;
 };
 
-const loadInitialAssets = async (bgAsset, doorAsset, handleAsset, handleShadowAsset) => {
+const loadAssets = async (bgAsset, doorAsset, handleAsset, handleShadowAsset, doorOpenAsset, doorOpenShadowAsset, blinkAsset) => {
     try {
         const assetsToLoad = [
             loadAsset(pathPrefix, bgAsset, fileExtension),
             loadAsset(pathPrefix, doorAsset, fileExtension),
             loadAsset(pathPrefix, handleAsset, fileExtension),
-            loadAsset(pathPrefix, handleShadowAsset, fileExtension)
+            loadAsset(pathPrefix, handleShadowAsset, fileExtension),
+            loadAsset(pathPrefix, doorOpenAsset, fileExtension),
+            loadAsset(pathPrefix, doorOpenShadowAsset, fileExtension),
+            loadAsset(pathPrefix, blinkAsset, fileExtension),
         ];
 
-        const [bgTexture, doorTexture, handleTexture, handleShadowTexture] = await Promise.all(assetsToLoad);
+        const [bgTexture, doorTexture, handleTexture, handleShadowTexture, doorOpenTexture, doorOpenShadowTexture, blinkTexture] = await Promise.all(assetsToLoad);
 
         background = new PIXI.Sprite(bgTexture);
         door = new PIXI.Sprite(doorTexture);
         handle = new PIXI.Sprite(handleTexture);
         handleShadow = new PIXI.Sprite(handleShadowTexture);
+        doorOpen = new PIXI.Sprite(doorOpenTexture);
+        doorOpenShadow = new PIXI.Sprite(doorOpenShadowTexture);
+        blink_texture = blinkTexture;
     } catch (error) {
         throw error;
     }
 };
 
 const updateBackground = () => {
-    background.anchor.set(0, 0);
-    background.width = app.renderer.width;
-    background.height = app.renderer.height;
+    background.anchor.set(0.5);
+
+    let scale = 1.0; // Default scale
+
+    // Check if it's on laptop or mobile landscape orientation
+    if (app.renderer.width > app.renderer.height || app.renderer.width >= 800) {
+        // Landscape orientation on mobile or laptop
+        scale = app.renderer.width / background.texture.width;
+    } else {
+        // Portrait orientation on mobile or smaller screens
+        const minDimension = Math.min(app.renderer.width, app.renderer.height); // Find the minimum dimension of the screen
+        scale = minDimension / Math.max(background.texture.width, background.texture.height); // Scale based on the smaller dimension
+    }
+
+    // Prevent elements from going off-screen on smaller devices
+    const maxScale = 1.0;
+    background.scale.set(Math.min(scale, maxScale));
+
+    background.position.set(app.renderer.width / 2, app.renderer.height / 2);
 };
 
 const updateDoor = () => {
     door.anchor.set(0.5);
-    door.width = app.renderer.width / 3;
-    door.height = app.renderer.height / 1.6;
-    door.position.set(app.renderer.width / 1.95, app.renderer.height / 2.05);
+    door.width = background.width / 3;
+    door.height = background.height / 1.6;
+    door.position.set(background.x * 1.015, background.y * 0.99);
 };
 
 const updateHandle = () => {
     handle.anchor.set(0.5);
-    handle.width = door.width / 3;
-    handle.height = door.height / 2.4;
-    handle.position.set(door.position.x - (door.width * 0.03046875), door.position.y);
+    handle.width = door.width * 0.35;
+    handle.height = door.height * 0.4;
+    handle.position.set(door.position.x * 0.97, door.position.y);
     handle.interactive = true;
 };
 
@@ -72,7 +130,7 @@ const updateHandleShadow = () => {
     handleShadow.anchor.set(0.5);
     handleShadow.width = handle.width;
     handleShadow.height = handle.height;
-    handleShadow.position.set(handle.position.x + ((handle.width * 1) / 100), handle.position.y);
+    handleShadow.position.set(handle.position.x * 1.01, handle.position.y);
 };
 
 const start = async () => {
@@ -88,11 +146,11 @@ const start = async () => {
     startGame();
 };
 
-const startGame = async () => {
+const startGame = async (winner) => {
     // Load initial assets
     const { bgAsset, doorAsset, doorOpenAsset, doorOpenShadowAsset, handleAsset, handleShadowAsset, blinkAsset } = assets;
     try {
-        await loadInitialAssets(bgAsset, doorAsset, handleAsset, handleShadowAsset);
+        await loadAssets(bgAsset, doorAsset, handleAsset, handleShadowAsset, doorOpenAsset, doorOpenShadowAsset, blinkAsset);
     } catch (error) {
         console.log("Failed to load assets!", error);
         return;
@@ -105,9 +163,17 @@ const startGame = async () => {
     app.stage.addChild(door);
     app.stage.addChild(handleShadow);
     app.stage.addChild(handle);
-
-    /* Animation */
-    const code = [{ rotations: 3, dir: 'clockwise' }, { rotations: 5, dir: 'counterclockwise' }, { rotations: 1, dir: 'clockwise' }];
+    if (winner) {
+        await closeDoor();
+        await lockSafe();
+        timeCounter = 0;
+        updateCounterText();
+    }
+    createCounterText();
+    startTimer();
+    /* Generate code */
+    const code = generateRandomCode();
+    console.log(code);
     let clicks = 1, counter = 0;
     handle.on('pointerdown', async (event) => {
         if (!handle.interactive) return; // If interaction is disabled -> return
@@ -117,7 +183,7 @@ const startGame = async () => {
         const clickPositionX = event.data.global.x - handle.x;
         const rotationSide = handle.rotation;
         const targetRotation = clickPositionX < 0 ? rotationSide - Math.PI / 3 : rotationSide + Math.PI / 3; // < 0 ? 60 degrees counterclockwise : 60 degrees clockwise
-        const curRotation = clickPositionX < 0 ? 'counterclockwise' : 'clockwise';
+        const curRotation = clickPositionX < 0 ? directions[1] : directions[0];
         // Animate rotation
         await Promise.all([
             gsap.to(handle, { rotation: targetRotation, duration: 1 }),
@@ -141,9 +207,11 @@ const initializeApp = async () => {
 
 const handleRotation = (code, clicks, counter, curRotation) => {
     if (isIncorrectDirection(code, counter, curRotation)) {
-        resetClicksAndCounter();
         console.log("Restarting...");
-        resetGame();
+        lockSafe().then(() => {
+            // After spinning, reset the game
+            resetGame(false);
+        });
     } else {
         if (isClickCountLessThanRequired(code, clicks, counter)) {
             clicks++;
@@ -152,8 +220,10 @@ const handleRotation = (code, clicks, counter, curRotation) => {
                 counter++;
                 clicks = 1;
             } else {
-                console.log("You Win!");
-                resetGame();
+                console.log(`You successfully unlocked the safe for ${formatTime(timeCounter)} seconds!`);
+                win().then(() => {
+                    resetGame(true);
+                })
             }
         }
     }
@@ -162,22 +232,115 @@ const handleRotation = (code, clicks, counter, curRotation) => {
 
 const isIncorrectDirection = (code, counter, curRotation) => code[counter].dir !== curRotation;
 
-const resetClicksAndCounter = () => {
-    clicks = 1;
-    counter = 0;
-};
-
 const isClickCountLessThanRequired = (code, clicks, counter) => clicks < code[counter].rotations;
 
 const isCounterWithinBounds = (code, counter) => counter < code.length - 1;
 
-const resetGame = () => {
+const resetGame = (winner) => {
     // Remove previous assets
     app.stage.removeChild(background);
-    app.stage.removeChild(door);
-    app.stage.removeChild(handleShadow);
-    app.stage.removeChild(handle);
+    app.stage.removeChild(timeText);
+    if (!winner) {
+        app.stage.removeChild(door);
+        app.stage.removeChild(handleShadow);
+        app.stage.removeChild(handle);
+    }
 
-    // Restart the game
-    startGame();
+    // Start the game
+    startGame(winner);
 };
+
+const generateRandomCode = () => {
+    return [
+        { rotations: Math.floor(Math.random() * 0) + 1, dir: directions[Math.floor(Math.random() * directions.length)] },
+        // { rotations: Math.floor(Math.random() * 9) + 1, dir: directions[Math.floor(Math.random() * directions.length)] },
+        // { rotations: Math.floor(Math.random() * 9) + 1, dir: directions[Math.floor(Math.random() * directions.length)] },
+    ];
+};
+
+const lockSafe = () => {
+    return new Promise((resolve) => {
+        gsap.to([handle, handleShadow], { rotation: handle.rotation + Math.PI * 12, duration: 4, onComplete: resolve });
+    });
+};
+
+const createBlink = () => {
+    const newBlink = new PIXI.Sprite(blink_texture);
+    newBlink.anchor.set(0.5);
+    newBlink.width = handle.width;
+    newBlink.height = newBlink.width;
+    return newBlink;
+};
+
+const glitterAnimation = (blink) => {
+    return gsap.timeline({ repeat: 1, repeatDelay: 0.5 })
+        .to(blink, { alpha: 0, duration: 1 })
+        .to(blink, { alpha: 1, duration: 1 });
+};
+
+const win = () => {
+    doorOpen.anchor.set(0.5);
+    doorOpen.position.set(door.x + door.width / 1.5, door.y);
+    doorOpen.scale.set(door.scale.x, door.scale.y);
+    doorOpen.alpha = 0;
+
+    doorOpenShadow.anchor.set(0.5);
+    doorOpenShadow.position.set(door.x + door.width / 1.4, door.y * 1.05);
+    doorOpenShadow.scale.set(door.scale.x, door.scale.y);
+    doorOpenShadow.alpha = 0;
+
+    blink1 = createBlink();
+    blink2 = createBlink();
+    blink3 = createBlink();
+    blink1.position.set(door.x * 0.81, door.y);
+    blink2.position.set(door.x * 0.96, door.y);
+    blink3.position.set(door.x * 1.04, door.y + (door.height * 0.25));
+
+    app.stage.addChild(blink1);
+    app.stage.addChild(blink2);
+    app.stage.addChild(blink3);
+    app.stage.addChild(doorOpenShadow);
+    app.stage.addChild(doorOpen);
+
+    glitterAnimation(blink1);
+    glitterAnimation(blink2);
+    glitterAnimation(blink3);
+
+    return new Promise((resolve) => gsap.to([handle, handleShadow, door], {
+        alpha: 0, duration: 1, onComplete: async function () {
+            app.stage.removeChild(handle);
+            app.stage.removeChild(handleShadow);
+            app.stage.removeChild(door);
+            await gsap.to([doorOpen, doorOpenShadow], { alpha: 1, duration: 1 });
+            resolve();
+        }
+    }));
+};
+
+const closeDoor = () => {
+    door.alpha = 0;
+    handle.alpha = 0;
+    handleShadow.alpha = 0;
+
+    return new Promise((resolve) => gsap.to([blink1, blink2, blink3, doorOpen, doorOpenShadow], {
+        alpha: 0, duration: 1, onComplete: async function () {
+            app.stage.removeChild(blink1);
+            app.stage.removeChild(blink2);
+            app.stage.removeChild(blink3);
+            app.stage.removeChild(doorOpen);
+            app.stage.removeChild(doorOpenShadow);
+            await gsap.to([door, handle, handleShadow], { alpha: 1, duration: 1 });
+            resolve();
+        }
+    }))
+};
+
+
+
+window.addEventListener('resize', () => {
+    updateBackground();
+    updateDoor();
+    updateHandle();
+    updateHandleShadow();
+    updateTimerStylesAndPosition();
+});
